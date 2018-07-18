1039add64a9ae5a7c63a3c8793c3f5f0d1c11ae1
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-07-18 16:39:32 -0400

    cleaning up calc_spillover_triangles

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -20354,69 +20354,69 @@ var Clustergrammer2 =
 
 	  var viz_dim = params.viz_dim;
 
-	  var mat_size = params.mat_size;
+	  var ini_mat = params.mat_size;
 
 	  var height_to_width = viz_dim.canvas.height/viz_dim.canvas.width;
-	  var scaled_height = {};
-	  scaled_height.x = mat_size.x / height_to_width;
-	  scaled_height.y = mat_size.y / height_to_width;
+	  var scaled_mat = {};
+	  scaled_mat.x = ini_mat.x / height_to_width;
+	  scaled_mat.y = ini_mat.y / height_to_width;
 
 
 	  var spillover_triangles = {};
 	  spillover_triangles.mat = [
 	    // left spillover rect
-	    {'pos': [[-1, 1], [-mat_size.x, -1], [-1.0, -1]]},
-	    {'pos': [[-1, 1], [-mat_size.x,  1], [-mat_size.x, -1]]},
+	    {'pos': [[-1, 1], [-ini_mat.x, -1], [-1.0, -1]]},
+	    {'pos': [[-1, 1], [-ini_mat.x,  1], [-ini_mat.x, -1]]},
 
 	    // right spillover rect
-	    {'pos': [[1, 1], [mat_size.x, -1], [1.0, -1]]},
-	    {'pos': [[1, 1], [mat_size.x,  1], [mat_size.x, -1]]},
+	    {'pos': [[1, 1], [ini_mat.x, -1], [1.0, -1]]},
+	    {'pos': [[1, 1], [ini_mat.x,  1], [ini_mat.x, -1]]},
 
 	    // top spillover rect
-	    {'pos': [[-mat_size.x, 1], [-mat_size.x, scaled_height.y], [mat_size.x, 1]]},
-	    {'pos': [[ mat_size.x, 1], [mat_size.x, scaled_height.y], [-mat_size.x, scaled_height.y]]},
+	    {'pos': [[-ini_mat.x, 1], [-ini_mat.x, scaled_mat.y], [ini_mat.x, 1]]},
+	    {'pos': [[ ini_mat.x, 1], [ini_mat.x, scaled_mat.y], [-ini_mat.x, scaled_mat.y]]},
 
 	    // // bottom spillover rect
-	    {'pos': [[-mat_size.x, -1], [-mat_size.x, -scaled_height.y], [mat_size.x, -1]]},
-	    {'pos': [[ mat_size.x, -1], [mat_size.x, -scaled_height.y], [-mat_size.x, -scaled_height.y]]},
+	    {'pos': [[-ini_mat.x, -1], [-ini_mat.x, -scaled_mat.y], [ini_mat.x, -1]]},
+	    {'pos': [[ ini_mat.x, -1], [ini_mat.x, -scaled_mat.y], [-ini_mat.x, -scaled_mat.y]]},
 	  ];
 
 	  spillover_triangles.mat_corners = [
 	    // top-left spillover rect
-	    {'pos': [[-1, 1], [-mat_size.x, scaled_height.y], [-1.0, scaled_height.y]]},
-	    {'pos': [[-1, 1], [-mat_size.x,  1], [-mat_size.x, scaled_height.y]]},
+	    {'pos': [[-1, 1], [-ini_mat.x, scaled_mat.y], [-1.0, scaled_mat.y]]},
+	    {'pos': [[-1, 1], [-ini_mat.x,  1], [-ini_mat.x, scaled_mat.y]]},
 
 	    // bottom-left spillover rect
-	    {'pos': [[-1, -1], [-mat_size.x, -scaled_height.y], [-1.0, -scaled_height.y]]},
-	    {'pos': [[-1, -1], [-mat_size.x,  -1], [-mat_size.x, -scaled_height.y]]},
+	    {'pos': [[-1, -1], [-ini_mat.x, -scaled_mat.y], [-1.0, -scaled_mat.y]]},
+	    {'pos': [[-1, -1], [-ini_mat.x,  -1], [-ini_mat.x, -scaled_mat.y]]},
 
 	    // top-right spillover rect
 	    // mat corners
-	    {'pos': [[1, 1], [mat_size.x, scaled_height.y], [1.0, scaled_height.y]]},
-	    {'pos': [[1, 1], [mat_size.x,  1], [mat_size.x, scaled_height.y]]},
+	    {'pos': [[1, 1], [ini_mat.x, scaled_mat.y], [1.0, scaled_mat.y]]},
+	    {'pos': [[1, 1], [ini_mat.x,  1], [ini_mat.x, scaled_mat.y]]},
 
 	    // bottom-right spillover rect
-	    {'pos': [[1, -1], [mat_size.x, -scaled_height.y], [1.0, -scaled_height.y]]},
-	    {'pos': [[1, -1], [mat_size.x,  -1], [mat_size.x, -scaled_height.y]]},
+	    {'pos': [[1, -1], [ini_mat.x, -scaled_mat.y], [1.0, -scaled_mat.y]]},
+	    {'pos': [[1, -1], [ini_mat.x,  -1], [ini_mat.x, -scaled_mat.y]]},
 
 	  ];
 
 	  spillover_triangles.label_corners = [
 	    // top-left spillover rect
-	    {'pos': [[-1, 1], [-mat_size.x, scaled_height.y], [-1.0, scaled_height.y]]},
-	    {'pos': [[-1, 1], [-mat_size.x,  1], [-mat_size.x, scaled_height.y]]},
+	    {'pos': [[-1, 1], [-ini_mat.x, scaled_mat.y], [-1.0, scaled_mat.y]]},
+	    {'pos': [[-1, 1], [-ini_mat.x,  1], [-ini_mat.x, scaled_mat.y]]},
 
 	    // bottom-left spillover rect
-	    {'pos': [[-1, -1], [-mat_size.x, -scaled_height.y], [-1.0, -scaled_height.y]]},
-	    {'pos': [[-1, -1], [-mat_size.x,  -1], [-mat_size.x, -scaled_height.y]]},
+	    {'pos': [[-1, -1], [-ini_mat.x, -scaled_mat.y], [-1.0, -scaled_mat.y]]},
+	    {'pos': [[-1, -1], [-ini_mat.x,  -1], [-ini_mat.x, -scaled_mat.y]]},
 
 	    // top-right spillover rect
 	    // label corners
-	    {'pos': [[1, scaled_height.y + (1-mat_size.x)], [mat_size.x, scaled_height.y], [1.0, scaled_height.y]]},
+	    {'pos': [[1, scaled_mat.y + (1-ini_mat.x)], [ini_mat.x, scaled_mat.y], [1.0, scaled_mat.y]]},
 
 	    // bottom-right spillover rect
-	    {'pos': [[1, -1], [mat_size.x, -scaled_height.y], [1.0, -scaled_height.y]]},
-	    {'pos': [[1, -1], [mat_size.x,  -1], [mat_size.x, -scaled_height.y]]},
+	    {'pos': [[1, -1], [ini_mat.x, -scaled_mat.y], [1.0, -scaled_mat.y]]},
+	    {'pos': [[1, -1], [ini_mat.x,  -1], [ini_mat.x, -scaled_mat.y]]},
 
 	  ];
 

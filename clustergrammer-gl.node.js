969f59a038ebd6a4972dccb285486667f0b9dd2c
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-07-24 12:01:14 -0400

    working on mouseover background positioning

--- clustergrammer-gl.node.js
+++ clustergrammer-gl.node.js
@@ -50975,22 +50975,20 @@ module.exports =
 
 	module.exports = function calc_tooltip_triangles(regl, params){
 
-	  var viz_dim = params.viz_dim;
+	  // var viz_dim = params.viz_dim;
 
-	  var ini_mat = params.mat_size;
-	  var ini_heat = params.heat_size;
+	  // var ini_mat = params.mat_size;
+	  // var ini_heat = params.heat_size;
 
-	  var height_to_width = viz_dim.canvas.height/viz_dim.canvas.width;
+	  // var height_to_width = viz_dim.canvas.height/viz_dim.canvas.width;
 
-	  var scaled_mat = {};
-	  scaled_mat.x = ini_mat.x / height_to_width;
-	  scaled_mat.y = ini_mat.y / height_to_width;
+	  // var scaled_mat = {};
+	  // scaled_mat.x = ini_mat.x / height_to_width;
+	  // scaled_mat.y = ini_mat.y / height_to_width;
 
-	  var scaled_heat = {};
-	  scaled_heat.x = ini_heat.x / height_to_width;
-	  scaled_heat.y = ini_heat.y / height_to_width;
-
-	  var spillover_triangles = {};
+	  // var scaled_heat = {};
+	  // scaled_heat.x = ini_heat.x / height_to_width;
+	  // scaled_heat.y = ini_heat.y / height_to_width;
 
 	  // trying to shift based on diff between mat and heat size
 	  var inst_shift = {}
@@ -50998,7 +50996,6 @@ module.exports =
 	  inst_shift.y = params.mat_size.y - params.heat_size.y;
 
 	  var background_triangles = [
-	    // top-left spillover rect
 	    {'pos': [[-1, 1],
 	             [-0.8 , 0.8],
 	             [-1.0 , 0.8]

ee08ede60c09071606b358ea414da2e447e8e565
cornhundred <nickfloresfernandez@gmail.com>
2018-07-26 21:14:13 -0400

    cleaning up make_row_text_triangle_args

--- src/make_row_text_triangle_args.js
+++ src/make_row_text_triangle_args.js
@@ -46,14 +46,12 @@ module.exports = function make_row_text_triangle_args(regl, params, zoom_functio
         //-----------------------------------------------
         // total_zoom stretches out row labels horizontally
         // then text is offset to the left side of the heatmap
-        x_position = position.x * total_zoom +
-                     x_offset * scale_text +
-                     // limited_scaling used to be total_zoom
-                     shift_text * limited_scaling;
+        x_position = position.x * total_zoom + x_offset * scale_text + shift_text * limited_scaling;
+        // x_position = position.x * total_zoom; // + x_offset * scale_text + shift_text * limited_scaling;
 
         // the y position varies for all row labels
         //-----------------------------------------------
-        y_position = -position.y + 2.0 * offset[1] * scale_text * heat_size - shift_heat * scale_text ;
+        y_position = -position.y + (2.0 * heat_size * offset[1] - shift_heat) * scale_text ;
 
         gl_Position = zoom *
                       vec4(

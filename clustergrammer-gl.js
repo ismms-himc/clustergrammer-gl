b7f3b7bafb46be99786136f79c8d768e31a5f9ad
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-08-08 10:39:01 -0400

    working on using control-panel

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -57455,11 +57455,25 @@ module.exports = function keep_track_of_mouseovers(params){
 // var filename = 'data/mult_view.json';
 
 var run_viz = __webpack_require__(/*! ./run_viz */ "./src/run_viz.js");
+var control = __webpack_require__(/*! control-panel */ "./node_modules/control-panel/index.js")
 
 // global variables
 // d3 = require('d3');
 
 function clustergrammer_gl(args){
+
+  // var panel = control([
+  //     // {type: 'range', label: 'my range', min: 0, max: 100, initial: 20},
+  //   // {type: 'range', label: 'log range', min: 0.1, max: 100, initial: 20, scale: 'log'},
+  //   // {type: 'text', label: 'my text', initial: 'my cool setting'},
+  //   // {type: 'checkbox', label: 'my checkbox', initial: true},
+  //   // {type: 'color', label: 'my color', format: 'rgb', initial: 'rgb(10,200,0)'},
+  //   // {type: 'button', label: 'gimme an alert', action: function () {alert('hello!');}},
+  //   // {type: 'select', label: 'select one', options: ['option 1', 'option 2'], initial: 'option 1'}
+  //   // {type: 'multibox', label: 'check many', count: 3, initial: [true, false, true]}
+  // ],
+  //   // {theme: 'light', position: 'top-right'}
+  // )
 
   console.log('################################');
   console.log('version 0.3.1');

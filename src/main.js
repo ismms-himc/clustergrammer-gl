a940d69303619c1fea2aef93a6503a8ba067b5b9
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-08-08 11:11:51 -0400

    added iffy-loader to package.json

--- src/main.js
+++ src/main.js
@@ -15,16 +15,16 @@ var control = require('control-panel')
 function clustergrammer_gl(args){
 
   var panel = control([
-      {type: 'range', label: 'my range', min: 0, max: 100, initial: 20},
+    {type: 'range', label: 'my range', min: 0, max: 100, initial: 20},
     // {type: 'range', label: 'log range', min: 0.1, max: 100, initial: 20, scale: 'log'},
-    // {type: 'text', label: 'my text', initial: 'my cool setting'},
+    {type: 'text', label: 'my text', initial: 'my cool setting'},
     // {type: 'checkbox', label: 'my checkbox', initial: true},
     // {type: 'color', label: 'my color', format: 'rgb', initial: 'rgb(10,200,0)'},
     // {type: 'button', label: 'gimme an alert', action: function () {alert('hello!');}},
-    // {type: 'select', label: 'select one', options: ['option 1', 'option 2'], initial: 'option 1'}
+    // {type: 'select', label: 'select one', options: ['option 1', 'option 2'], initial: 'option 1'},
     // {type: 'multibox', label: 'check many', count: 3, initial: [true, false, true]}
   ],
-    // {theme: 'light', position: 'top-right'}
+    {theme: 'light', position: 'top-left'}
   )
 
   console.log('################################');

eb8b2c060bd88dac20544c139d23b6a32a3d0f4a
cornhundred <nickfloresfernandez@gmail.com>
2018-08-13 17:21:05 -0400

    working on on-the-fly reordering calculations of position array

--- src/reorder_panel.js
+++ src/reorder_panel.js
@@ -35,8 +35,15 @@ module.exports = function reorder_panel(params, control_container, inst_axis){
   );
 
   panel_1.on('input', function(data){
-    console.log('something happening', data)
+      console.log('something happening', data)
       params.animation.run_switch = true;
+      params.new_order.col = data['row Order'];
+      console.log(params.new_order.col)
+
+      /*
+      Need to calcualte new position array when choosing new order
+      */
+
   })
 
 };

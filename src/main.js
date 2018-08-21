0505694230b1402dd18890571d9b254828c05c6f
cornhundred <nickfloresfernandez@gmail.com>
2018-08-21 10:55:37 -0400

    set up row reordering, working on reordering row/col nodes and upgrading Jupyter Glidget, then dendrogram

--- src/main.js
+++ src/main.js
@@ -57,29 +57,37 @@ function clustergrammer_gl(args){
 
   control_panels.col.on('input', function(data){
 
-      console.log('something happening', data)
+      console.log('reordering columns', data)
       params.animation.run_switch = true;
-      // params.new_order.row = data['row Order'];
       params.new_order.col = data['col Order'];
-
-      // console.log(params.new_order.row, params.new_order.col)
 
       params.arrs.position_arr['new'] = make_position_arr(params,
                                       params.new_order.row,
                                       params.new_order.col);
-
-      // var new_pos_arr = params.arrs.position_arr['new']
 
       params.matrix_args.regl_props.rects.attributes.pos_att_new = {
             buffer: regl.buffer(params.arrs.position_arr['new']),
             divisor: 1
           };
 
-      /*
-      Need to calcualte new position array when choosing new order
-      */
+  });
 
-  })
+  control_panels.row.on('input', function(data){
+
+      console.log('reordering rows', data)
+      params.animation.run_switch = true;
+      params.new_order.row = data['row Order'];
+
+      params.arrs.position_arr['new'] = make_position_arr(params,
+                                      params.new_order.row,
+                                      params.new_order.col);
+
+      params.matrix_args.regl_props.rects.attributes.pos_att_new = {
+            buffer: regl.buffer(params.arrs.position_arr['new']),
+            divisor: 1
+          };
+
+  });
 
   return cgm;
 

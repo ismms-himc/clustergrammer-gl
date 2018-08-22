c492c867853d42ffafea809f4ceb6702750e10ba
cornhundred <nickfloresfernandez@gmail.com>
2018-08-21 22:41:34 -0400

    working on pre-calc category positions

--- clustergrammer-gl.node.js
+++ clustergrammer-gl.node.js
@@ -33957,9 +33957,9 @@ module.exports = function make_cat_args(regl, params, inst_rc, cat_index){
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = function make_cat_position_array(params, cat_index, inst_rc){
+module.exports = function make_cat_position_array(params, cat_index, inst_axis){
 
-  var num_labels = params['num_'+inst_rc];
+  var num_labels = params['num_'+inst_axis];
   // category tiles have fixed heights
   var cat_height;
   // category widths depend on the number of labels
@@ -33967,7 +33967,7 @@ module.exports = function make_cat_position_array(params, cat_index, inst_rc){
   var mat_size;
   var top_shift_triangles;
   cat_height = 0.04;
-  if (inst_rc === 'col'){
+  if (inst_axis === 'col'){
     mat_size = params.heat_size.x;
     top_shift_triangles = params.mat_size.y;
     cat_width = (mat_size/0.5)/num_labels;
@@ -33988,7 +33988,7 @@ module.exports = function make_cat_position_array(params, cat_index, inst_rc){
   // console.log('shift_cat', shift_cat)
   var top_offset = -top_shift_triangles - cat_height + shift_cat;
 
-  var inst_order = params.inst_order[inst_rc];
+  var inst_order = params.inst_order[inst_axis];
 
   var y_offset_array = [];
   var i;
@@ -33997,12 +33997,12 @@ module.exports = function make_cat_position_array(params, cat_index, inst_rc){
     // emperically found rules
     var order_id;
     var shift_mat_heat;
-    if (inst_rc == 'row'){
-      order_id = num_labels - params.network[inst_rc + '_nodes'][i][inst_order] - 1;
+    if (inst_axis == 'row'){
+      order_id = num_labels - params.network[inst_axis + '_nodes'][i][inst_order] - 1;
       // vertical shift
       shift_mat_heat = - (params.mat_size.y - params.heat_size.y)
     } else {
-      order_id = params.network[inst_rc + '_nodes'][i][inst_order] ;
+      order_id = params.network[inst_axis + '_nodes'][i][inst_order] ;
       shift_mat_heat = params.mat_size.x - params.heat_size.x
     }
 
@@ -34010,7 +34010,7 @@ module.exports = function make_cat_position_array(params, cat_index, inst_rc){
     // the last part is necessary to shfit the viz aid triangles down to make up for the smaller size
     // of the heatmap vs the general matrix area
 
-    // console.log(inst_rc, 'shift_mat_heat', shift_mat_heat)
+    // console.log(inst_axis, 'shift_mat_heat', shift_mat_heat)
     y_offset_array[i] = mat_size - cat_width/2 - order_id * cat_width + shift_mat_heat;
   }
 

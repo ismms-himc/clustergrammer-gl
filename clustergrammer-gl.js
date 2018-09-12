0618f01f7a34ac05faad069ba859fa4ae68ccc5c
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-09-12 13:42:15 -0400

    making multiple buttons using data-binding in d3

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -22420,17 +22420,42 @@ module.exports = function build_control_panel(regl, cgm){
   button_dim.height = 30;
   button_dim.width = 100;
 
-  control_svg
+  var button_offset
+
+  var button_group = control_svg
+    .selectAll('g')
+    .data(['Clust', 'Sum'])
+    .enter( )
+    .append('g')
+    .attr('transform', function(d, i){
+      var x_offset = 115 * i + 25;
+      return 'translate('+ x_offset  +', '+ 25 +')';
+    })
+    .on('click', function(){
+      run_reorder(regl, cgm);
+    })
+
+  button_group
     .append('rect')
     .style('height', button_dim.height)
     .style('width', button_dim.width)
-    .attr('transform', 'translate(50, 50)')
     .style('fill', '  #778899')
     .style('rx', 10)
-    .style('ry', 10)
-    .on('click', function(){
-      run_reorder(regl, cgm);
-    });
+    .style('ry', 10);
+
+  button_group
+    .append('text')
+    .classed('cat_graph_title', true)
+    .text(function(d){
+      return d;
+    })
+    .style('font-family', '"Helvetica Neue", Helvetica, Arial, sans-serif')
+    // .style('font-weight',  500)
+    .style('font-size', 15)
+    .style('text-anchor', 'middle')
+    .style('alignment-baseline', 'middle')
+    .style('cursor', 'default')
+    .attr('transform', 'translate('+ button_dim.width/2 +', '+ button_dim.height/2 +')');
 
       // <rect x="20" y="20" width="50" height="50" rx="5" ry="5" stroke='red' stroke-width="2" fill="white"/>
 

1999a14c3992b1d2ade408721f69969d35a61d49
cornhundred <nickfloresfernandez@gmail.com>
2018-09-12 14:06:03 -0400

    version 0.5.1-dev about to publish

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -24127,7 +24127,7 @@ var build_dendrogram_sliders = __webpack_require__(/*! ./dendrogram/build_dendro
 function clustergrammer_gl(args){
 
   console.log('################################');
-  console.log('clustergrammer-gl version 0.5.0 - dev!');
+  console.log('clustergrammer-gl version 0.5.1 - dev!');
   console.log('################################');
 
   var network = args.network;
--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ var build_dendrogram_sliders = require('./dendrogram/build_dendrogram_sliders')
 function clustergrammer_gl(args){
 
   console.log('################################');
-  console.log('clustergrammer-gl version 0.5.0 - dev!');
+  console.log('clustergrammer-gl version 0.5.1 - dev!');
   console.log('################################');
 
   var network = args.network;

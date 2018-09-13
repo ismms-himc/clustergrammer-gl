20928b3f6808dc955f9d87181c5323fd4309e300
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-09-13 14:18:44 -0400

    fixed row cat animation

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -26013,16 +26013,23 @@ module.exports = function reorder_cat_args(regl, cgm){
 
   var params = cgm.params;
 
-  // update cat position arrays
-  for (var cat_index = 0; cat_index < params.cat_num.col; cat_index++) {
-    params.cat_arrs.new.col[cat_index] = make_cat_position_array(params, 'col', cat_index, params.new_order.col);
+  // can make more efficient by only checking which axis needs to be reordered
 
-    // update the attribute
-    params.cat_args.col[cat_index].attributes.cat_pos_att_new = {
-        buffer: regl.buffer(params.cat_arrs.new.col[cat_index]),
-        divisor: 1
-    };
-  }
+  _.each(['row', 'col'], function(inst_axis){
+
+    // update cat position arrays
+    for (var cat_index = 0; cat_index < params.cat_num[inst_axis]; cat_index++) {
+      params.cat_arrs.new[inst_axis][cat_index] = make_cat_position_array(params, inst_axis, cat_index, params.new_order[inst_axis]);
+
+      // update the attribute
+      params.cat_args[inst_axis][cat_index].attributes.cat_pos_att_new = {
+          buffer: regl.buffer(params.cat_arrs.new[inst_axis][cat_index]),
+          divisor: 1
+      };
+    }
+
+  });
+
 
 };
 

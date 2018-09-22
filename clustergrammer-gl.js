7ce4581c72e6b0d701f13fd8c07478e1bed61547
cornhundred <nickfloresfernandez@gmail.com>
2018-09-22 18:42:39 -0400

    cleaned get_ordered_laels

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -24514,44 +24514,33 @@ module.exports = function get_ordered_labels(params){
   var inst_order;
   var ordered_labels = {};
 
-  var row_nodes = params.network.row_nodes;
   var col_nodes = params.network.col_nodes;
   ordered_labels.rows = [];
   ordered_labels.cols = [];
 
   // only showing col cat in mouseover for now
 
-  var found_row_cat = false;
-  ordered_labels.row_cats = [];
-  if (params.cat_num.row > 0){
-    found_row_cat = true;
-  }
+  var axis_nodes;
+  _.each(['row', 'col'], function(inst_axis){
 
-  _.each(row_nodes, function(inst_node){
+    axis_nodes = params.network[inst_axis + '_nodes'];
 
-    inst_order = params.num_row - 1 - inst_node[params.inst_order.row];
-    ordered_labels.rows[inst_order] = inst_node.name;
-
-    if (found_row_cat){
-      ordered_labels.row_cats[inst_order] = inst_node['cat-0'];
+    var found_axis_cat = false;
+    ordered_labels[inst_axis + '_cats'] = [];
+    if (params.cat_num[inst_axis] > 0){
+      found_axis_cat = true;
     }
 
-  });
+    _.each(axis_nodes, function(inst_node){
 
-  var found_col_cat = false;
-  ordered_labels.col_cats = [];
-  if (params.cat_num.col > 0){
-    found_col_cat = true;
-  }
+      inst_order = params['num_' + inst_axis] - 1 - inst_node[params.inst_order[inst_axis]];
+      ordered_labels[inst_axis + 's'][inst_order] = inst_node.name;
 
-  _.each(col_nodes, function(inst_node){
+      if (found_axis_cat){
+        ordered_labels[inst_axis + '_cats'][inst_order] = inst_node['cat-0'];
+      }
 
-    inst_order = params.num_col- 1 - inst_node[params.inst_order.col];
-    ordered_labels.cols[inst_order] = inst_node.name;
-
-    if (found_col_cat){
-      ordered_labels.col_cats[inst_order] = inst_node['cat-0'];
-    }
+    });
 
   });
 

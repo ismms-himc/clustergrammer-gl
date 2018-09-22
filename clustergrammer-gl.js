8bd6e60485c15230462157800b7532a21a7737be
cornhundred <nickfloresfernandez@gmail.com>
2018-09-22 18:38:17 -0400

    cleaning get_ordered_labels

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -24511,6 +24511,7 @@ module.exports = function calc_text_triangles(params, inst_axis){
 
 module.exports = function get_ordered_labels(params){
 
+  var inst_order;
   var ordered_labels = {};
 
   var row_nodes = params.network.row_nodes;
@@ -24519,22 +24520,33 @@ module.exports = function get_ordered_labels(params){
   ordered_labels.cols = [];
 
   // only showing col cat in mouseover for now
-  ordered_labels.col_cats = [];
 
-  var inst_order;
+  var found_row_cat = false;
+  ordered_labels.row_cats = [];
+  if (params.cat_num.row > 0){
+    found_row_cat = true;
+  }
+
   _.each(row_nodes, function(inst_node){
+
     inst_order = params.num_row - 1 - inst_node[params.inst_order.row];
     ordered_labels.rows[inst_order] = inst_node.name;
+
+    if (found_row_cat){
+      ordered_labels.row_cats[inst_order] = inst_node['cat-0'];
+    }
+
   });
 
   var found_col_cat = false;
+  ordered_labels.col_cats = [];
   if (params.cat_num.col > 0){
     found_col_cat = true;
   }
 
   _.each(col_nodes, function(inst_node){
-    inst_order = params.num_col- 1 - inst_node[params.inst_order.col];
 
+    inst_order = params.num_col- 1 - inst_node[params.inst_order.col];
     ordered_labels.cols[inst_order] = inst_node.name;
 
     if (found_col_cat){
--- src/matrix_labels/get_ordered_labels.js
+++ src/matrix_labels/get_ordered_labels.js
@@ -1,5 +1,6 @@
 module.exports = function get_ordered_labels(params){
 
+  var inst_order;
   var ordered_labels = {};
 
   var row_nodes = params.network.row_nodes;
@@ -8,22 +9,33 @@ module.exports = function get_ordered_labels(params){
   ordered_labels.cols = [];
 
   // only showing col cat in mouseover for now
-  ordered_labels.col_cats = [];
 
-  var inst_order;
+  var found_row_cat = false;
+  ordered_labels.row_cats = [];
+  if (params.cat_num.row > 0){
+    found_row_cat = true;
+  }
+
   _.each(row_nodes, function(inst_node){
+
     inst_order = params.num_row - 1 - inst_node[params.inst_order.row];
     ordered_labels.rows[inst_order] = inst_node.name;
+
+    if (found_row_cat){
+      ordered_labels.row_cats[inst_order] = inst_node['cat-0'];
+    }
+
   });
 
   var found_col_cat = false;
+  ordered_labels.col_cats = [];
   if (params.cat_num.col > 0){
     found_col_cat = true;
   }
 
   _.each(col_nodes, function(inst_node){
-    inst_order = params.num_col- 1 - inst_node[params.inst_order.col];
 
+    inst_order = params.num_col- 1 - inst_node[params.inst_order.col];
     ordered_labels.cols[inst_order] = inst_node.name;
 
     if (found_col_cat){

4438cb6f001f5c5ae058e8485d0a62fca5435d32
cornhundred <nickfloresfernandez@gmail.com>
2018-09-25 12:34:28 -0400

    cleaning draw_background_calculations, working on improved text calc and gathering

--- src/draws/draw_background_calculations.js
+++ src/draws/draw_background_calculations.js
@@ -12,8 +12,10 @@ module.exports = function draw_background_calculations(regl, params){
 
   */
 
-  var updated_labels = false;
+  // var updated_labels = false;
   _.each(['row', 'col'], function(inst_axis){
+
+    // high priority queue
     if (params.label_queue.high[inst_axis].length > 0){
 
       var inst_name = params.label_queue.high[inst_axis][0];
@@ -21,24 +23,30 @@ module.exports = function draw_background_calculations(regl, params){
       if (inst_name in params.text_triangles[inst_axis] === false){
 
         params.text_triangles[inst_axis][inst_name] = vectorize_label(params, inst_axis, inst_name);
-        updated_labels = true;
+        // updated_labels = true;
 
-        // console.log(inst_name, params.label_queue.high[inst_axis].length)
+        console.log(inst_name, params.label_queue.high[inst_axis].length)
 
         /*
-          updated the text_triangles axis, but need to update the draw
+          updated the text_triangles axis, might not update the gathered draw triangles
         */
 
       } else {
         // console.log('already found triangle', params.label_queue.high[inst_axis].length)
       }
     }
+
   });
 
-  // run draw in the same loop, do not wait until next animation loop
-  if (updated_labels){
-    // console.log('draw updated labels')
-    draw_commands(regl, params);
-  }
+  /*
+  Do not need to draw when updating the text_triangles, only need to draw
+  when updating the gathered draw text triangles
+  */
+
+  // // run draw in the same loop, do not wait until next animation loop
+  // if (updated_labels){
+  //   // console.log('draw updated labels')
+  //   draw_commands(regl, params);
+  // }
 
 };

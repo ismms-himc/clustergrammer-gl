dcfe5fd010d0b9cd67561c9e6fd84802f3de164f
cornhundred <nickfloresfernandez@gmail.com>
2018-09-27 00:26:12 -0400

    cleaning make_position_arr

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -24728,16 +24728,15 @@ module.exports = function make_position_arr(params, inst_row_order, inst_col_ord
   // generate x position array
   var x_arr = Array(num_col).fill()
     .map(function(_, i){
-      return   i/num_col * (params.viz_dim.heat_size.x/0.5) + offset.x - 2*params.viz_dim.heat_size.x;
+      // return   2 * params.viz_dim.heat_size.x * i/num_col - 2 * params.viz_dim.heat_size.x + offset.x;
+      return      2 * params.viz_dim.heat_size.x * ( i/num_col - 1 ) + offset.x;
     });
 
   var y_arr = Array(num_row).fill()
     .map(function(_, i){
 
       // updated to take into consideration params.viz_dim.heat_size.x
-   // return -i/num_row * (params.viz_dim.heat_size.y/0.5) + offset.y - 1/num_row /(0.5/params.viz_dim.heat_size.y) - 2*(offset.y - params.viz_dim.heat_size.y) ;
-      // return -i/num_row * (params.viz_dim.heat_size.y/0.5) - offset.y - 2*params.viz_dim.heat_size.y/num_row + 2*params.viz_dim.heat_size.y;
-      return -(i/num_row * (params.viz_dim.heat_size.y/0.5) + offset.y + 2*params.viz_dim.heat_size.y * (1/num_row - 1));
+      return -(2 * params.viz_dim.heat_size.y * i/num_row - 2 * params.viz_dim.heat_size.y * (1 - 1/num_row) + offset.y);
     });
 
   params.node_canvas_pos = {};

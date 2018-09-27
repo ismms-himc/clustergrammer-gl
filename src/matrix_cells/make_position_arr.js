7dc2b67d19f3e78472b0298cbb68f96481015f0c
cornhundred <nickfloresfernandez@gmail.com>
2018-09-27 09:43:29 -0400

    cleaned x_arr and y_arr generation

--- src/matrix_cells/make_position_arr.js
+++ src/matrix_cells/make_position_arr.js
@@ -14,29 +14,66 @@ module.exports = function make_position_arr(params, inst_row_order, inst_col_ord
 
   // generate x position array
   params.node_canvas_pos = {};
+  var inst_pos;
+  var inst_heat_size
+  var num_labels;
   var inst_index;
+  var inst_offset;
+  var inst_direct;
 
-  // _.each(['x', 'y'], function(inst_axis){
+  _.each(['x', 'y'], function(inst_axis){
 
-  //   params.node_canvas_pos[inst_axis + '_arr'] = Array(num_col).fill()
-  //     .map(function(_, i){
-  //       inst_index = i;
-  //       return  2.0 * params.viz_dim.heat_size.x * (inst_index/num_col - 1) + offset.x;
-  //     });
+    if (inst_axis == 'x'){
+      num_labels = num_col;
+    } else {
+      num_labels = num_row;
+    }
 
-  // });
+    params.node_canvas_pos[inst_axis + '_arr'] = Array(num_labels).fill()
+      .map(function(_, i){
 
-  params.node_canvas_pos.x_arr = Array(num_col).fill()
-    .map(function(_, i){
-      inst_index = i;
-      return  2.0 * params.viz_dim.heat_size.x * (inst_index/num_col - 1) + offset.x;
-    });
+        if (inst_axis === 'x'){
+          inst_index = i;
+          inst_direct = 1;
+          num_labels = num_col;
+        } else {
+          inst_index = i + 1;
+          inst_direct = -1;
+          num_labels = num_row;
+        }
 
-  params.node_canvas_pos.y_arr = Array(num_row).fill()
-    .map(function(_, i){
-      inst_index = i + 1;
-      return -2.0 * params.viz_dim.heat_size.y * (inst_index/num_row - 1)  - offset.y;
-    });
+        inst_offset = offset[inst_axis]
+        inst_heat_size = params.viz_dim.heat_size[inst_axis];
+        inst_pos = 2.0 * inst_heat_size * (inst_index/num_labels - 1) + inst_offset;
+        return  inst_pos * inst_direct;
+      });
+
+  });
+
+
+  // params.node_canvas_pos.x_arr = Array(num_col).fill()
+  //   .map(function(_, i){
+  //     inst_index = i;
+  //     inst_direct = 1;
+  //     num_labels = num_col;
+
+  //     inst_offset = offset.x
+  //     inst_heat_size = params.viz_dim.heat_size.x;
+  //     inst_pos = 2.0 * inst_heat_size * (inst_index/num_labels - 1) + inst_offset;
+  //     return  inst_pos * inst_direct;
+  //   });
+
+  // params.node_canvas_pos.y_arr = Array(num_row).fill()
+  //   .map(function(_, i){
+  //     inst_index = i + 1;
+  //     inst_direct = -1;
+  //     num_labels = num_row;
+
+  //     inst_offset = offset.y;
+  //     inst_heat_size = params.viz_dim.heat_size.y;
+  //     inst_pos = 2.0 * inst_heat_size * (inst_index/num_labels - 1) + inst_offset;
+  //     return inst_pos * inst_direct;
+  //   });
 
   // pass along row and col node information
   var row_nodes = network.row_nodes;

57063657670a7e1b774a2375c0cd6fc1f6bf6e33
cornhundred <nickfloresfernandez@gmail.com>
2018-09-26 23:11:41 -0400

    organizing dendro

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -25930,9 +25930,17 @@ var make_dendro_args = __webpack_require__(/*! ./../dendrogram/make_dendro_args 
 module.exports = function generate_dendro_params(regl, params){
 
   params.dendro = {};
+
+  params.dendro.default_level = 5;
+
   params.dendro.dendro_args = {};
+  params.dendro.group_level = {};
+
   _.each(['row', 'col'], function(inst_axis){
+
     params.dendro.dendro_args[inst_axis] = make_dendro_args(regl, params, inst_axis);
+    params.dendro.group_level[inst_axis] = params.dendro.default_level;
+
   });
 
 };

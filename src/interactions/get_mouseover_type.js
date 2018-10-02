e78661b62819dfb2e7da16ab4759f539df772e74
cornhundred <nickfloresfernandez@gmail.com>
2018-10-02 01:16:15 -0400

    switching to absolute cursor position to determine mouseover type

--- src/interactions/get_mouseover_type.js
+++ src/interactions/get_mouseover_type.js
@@ -1,42 +1,51 @@
 module.exports = function get_mouseover_type(params, cursor_rel_min){
 
+  // switch to using absolute cursor position to determine mouseover type
+
+  console.log(params.zoom_data.x.cursor_position, params.zoom_data.y.cursor_position)
+
+
   var viz_dim_heat = params.viz_dim.heat;
+
+  var effective_max_width = viz_dim_heat.width + params.zoom_data.x.total_pan_min;
+  var effective_max_height = viz_dim_heat.height + params.zoom_data.y.total_pan_min
 
   params.tooltip.in_bounds_tooltip = false;
   // matrix cell
   if (cursor_rel_min.x > 0 &&
-      cursor_rel_min.x < viz_dim_heat.width &&
+      cursor_rel_min.x < effective_max_width &&
       cursor_rel_min.y > 0 &&
-      cursor_rel_min.y < viz_dim_heat.height){
+      cursor_rel_min.y < effective_max_height){
     params.tooltip.in_bounds_tooltip = true;
     params.tooltip.tooltip_type = 'matrix-cell';
 
   // row label
   } else if (cursor_rel_min.x < 0 &&
-             cursor_rel_min.y < viz_dim_heat.height){
+             cursor_rel_min.y < effective_max_height){
 
     params.tooltip.tooltip_type = 'row-label';
 
   // col label
   } else if (cursor_rel_min.y < 0 &&
-             cursor_rel_min.x < viz_dim_heat.width){
+             cursor_rel_min.x < effective_max_width){
 
     params.tooltip.tooltip_type = 'col-label';
 
   // row dendro
-  } else if (cursor_rel_min.x > viz_dim_heat.width &&
+  } else if (cursor_rel_min.x > effective_max_width &&
              cursor_rel_min.y > 0 &&
-             cursor_rel_min.y < viz_dim_heat.height){
+             cursor_rel_min.y < effective_max_height){
 
     params.tooltip.tooltip_type = 'row-dendro';
 
   // col dendro
-  } else if (cursor_rel_min.y > viz_dim_heat.height &&
+  } else if (cursor_rel_min.y > effective_max_height &&
              cursor_rel_min.x > 0 &&
-             cursor_rel_min.x < viz_dim_heat.height){
+             cursor_rel_min.x < effective_max_width){
 
     params.tooltip.tooltip_type = 'col-dendro';
 
   }
+
 
 }

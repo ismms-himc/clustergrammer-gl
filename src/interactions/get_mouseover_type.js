1dd2e8d4a34ce3cb467bc10e554971a150889edb
cornhundred <nickfloresfernandez@gmail.com>
2018-10-02 01:03:41 -0400

    working on mouseover type

--- src/interactions/get_mouseover_type.js
+++ src/interactions/get_mouseover_type.js
@@ -17,11 +17,26 @@ module.exports = function get_mouseover_type(params, cursor_rel_min){
 
     params.tooltip.tooltip_type = 'row-label';
 
-  // row label
+  // col label
   } else if (cursor_rel_min.y < 0 &&
              cursor_rel_min.x < viz_dim_heat.width){
 
     params.tooltip.tooltip_type = 'col-label';
 
+  // row dendro
+  } else if (cursor_rel_min.x > viz_dim_heat.width &&
+             cursor_rel_min.y > 0 &&
+             cursor_rel_min.y < viz_dim_heat.height){
+
+    params.tooltip.tooltip_type = 'row-dendro';
+
+  // col dendro
+  } else if (cursor_rel_min.y > viz_dim_heat.height &&
+             cursor_rel_min.x > 0 &&
+             cursor_rel_min.x < viz_dim_heat.height){
+
+    params.tooltip.tooltip_type = 'col-dendro';
+
   }
+
 }

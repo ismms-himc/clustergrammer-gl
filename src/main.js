8238fdf61f3b53283a73c8a0ea261cb131bdb790
Nick Fernandez <nickfloresfernandez@gmail.com>
2018-10-15 13:45:25 -0400

    able to decompress mult_view but getting error with larger files

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@
 var run_viz = require('./draws/run_viz');
 var build_control_panel = require('./control_panel/build_control_panel');
 var build_dendrogram_sliders = require('./dendrogram/build_dendrogram_sliders')
+var pako = require('pako');
 
 function clustergrammer_gl(args){
 
@@ -14,7 +15,45 @@ function clustergrammer_gl(args){
   // console.log('clustergrammer-gl version 0.6.3');
   // console.log('################################');
 
-  var network = args.network;
+  console.log('loading pako')
+  console.log(pako)
+
+  // debugger
+
+  // comp_net = JSON.parse(args.network).compressed;
+
+  // Decode base64 (convert ascii to binary)
+
+  comp_net = JSON.parse(args.network).compressed
+  var strData     = atob(comp_net);
+
+  console.log(strData)
+
+  // Convert binary string to character-number array
+  var charData    = strData.split('').map(function(x){return x.charCodeAt(0);});
+
+  console.log(charData)
+
+  // Turn number array into byte-array
+  var binData     = new Uint8Array(charData);
+
+  console.log(binData)
+
+  // Pako magic
+  var data        = pako.inflate(binData);
+
+  console.log(data)
+
+  // Convert gunzipped byteArray back to ascii string:
+  var strData     = String.fromCharCode.apply(null, new Uint16Array(data));
+
+  parse_1 = JSON.parse(strData)
+  uncomp_net = JSON.parse(parse_1)
+
+  // console.log(uncomp_net)
+
+  // var network = args.network;
+  var network = uncomp_net;
   var container = args.container;
 
   // make control panel first so it appears above canvas

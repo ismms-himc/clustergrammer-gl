fe90550378fcdf5c5555b07e15aee1576d92db75
cornhundred <nickfloresfernandez@gmail.com>
2018-11-06 20:28:54 -0500

    improving show_d3_tip

--- src/tooltip/show_d3_tip.js
+++ src/tooltip/show_d3_tip.js
@@ -13,9 +13,9 @@ module.exports = function show_d3_tip(params){
 
     d3.select(params.tooltip_id)
       .html(function(){
-        var full_string = mouseover.row.name + ' and ' + mouseover.col.name + ' <br> ' +
+        var full_string = mouseover.row.name + ' and ' +
+                          mouseover.col.name + ' <br> ' +
                           'value: ' + mouseover.value.toFixed(3);
-
         return full_string;
       });
 
@@ -24,48 +24,42 @@ module.exports = function show_d3_tip(params){
   var cat_breakdown;
 
   if (params.tooltip.tooltip_type === 'col-dendro'){
-
-
-    params.tooltip_fun.show('tooltip');
-
-    console.log('working on col dendro')
-
-    cat_breakdown = calc_cat_cluster_breakdown(params, mouseover.col.dendro, 'col');
-
-    var group_tooltip_container = d3.select(params.tooltip_id).node();
-
-    make_cat_breakdown_graph(params, mouseover.col.dendro, cat_breakdown, 'col', group_tooltip_container)
-
-
-
-
+    make_dendro_tooltip('col');
+  } else if (params.tooltip.tooltip_type === 'row-dendro') {
+    make_dendro_tooltip('row');
   }
 
+  function make_dendro_tooltip(inst_axis){
 
-    var d3_tip_width = parseFloat(d3.select(params.tooltip_id)
-                                 .style('width')
-                                 .replace('px',''));
+    params.tooltip_fun.show('tooltip');
+    cat_breakdown = calc_cat_cluster_breakdown(params, mouseover[inst_axis].dendro, inst_axis);
+    var group_tooltip_container = d3.select(params.tooltip_id).node();
+    make_cat_breakdown_graph(params, mouseover[inst_axis].dendro, cat_breakdown, inst_axis, group_tooltip_container)
+  }
 
-    var d3_tip_height = parseFloat(d3.select(params.tooltip_id)
-                                 .style('height')
-                                 .replace('px',''));
+  // position tooltip
+  var d3_tip_width = parseFloat(d3.select(params.tooltip_id)
+                               .style('width')
+                               .replace('px',''));
 
-    // this is necessary to offset hte tooltip correctly, probably due to the
-    // padding in the tooltip or some related paramters
-    var magic_x_offset = 22;
+  var d3_tip_height = parseFloat(d3.select(params.tooltip_id)
+                               .style('height')
+                               .replace('px',''));
 
-    params.d3_tip_width = d3_tip_width
+  // this is necessary to offset hte tooltip correctly, probably due to the
+  // padding in the tooltip or some related paramters
+  var magic_x_offset = 22;
 
-    d3.select(params.tooltip_id)
-      .style('margin-left', function(){
-        var total_x_offset = params.zoom_data.x.cursor_position - d3_tip_width + 22;
-        return total_x_offset + 'px'
-      })
-      .style('margin-top', function(){
-        var total_y_offset = params.zoom_data.y.cursor_position - d3_tip_height;
-        return total_y_offset + 'px'
-      });
+  params.d3_tip_width = d3_tip_width
 
-
+  d3.select(params.tooltip_id)
+    .style('margin-left', function(){
+      var total_x_offset = params.zoom_data.x.cursor_position - d3_tip_width + 22;
+      return total_x_offset + 'px'
+    })
+    .style('margin-top', function(){
+      var total_y_offset = params.zoom_data.y.cursor_position - d3_tip_height;
+      return total_y_offset + 'px'
+    });
 
 }

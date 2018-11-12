26c21adc15d0623b22a89b595f963847ee1f151a
cornhundred <nickfloresfernandez@gmail.com>
2018-11-12 13:48:35 -0500

    clustergrammer-gl 0.7.1 fixed col reordering bug when no title present

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -43064,7 +43064,7 @@ module.exports = function track_interaction_zoom_data(regl, params, ev){
 
 /*
 
-  clustergrammer-gl version 0.7.0
+  clustergrammer-gl version 0.7.1
 
  */
 
@@ -43076,7 +43076,7 @@ var pako = __webpack_require__(/*! pako */ "./node_modules/pako/index.js");
 function clustergrammer_gl(args){
 
   console.log('################################');
-  console.log('clustergrammer-gl version 0.7.0');
+  console.log('clustergrammer-gl version 0.7.1');
   console.log('################################');
 
   // decompress if necessary

1b5e2f1b396b2d566cf7a74f66b6f444d1afbb29
Nick Fernandez <nickfloresfernandez@gmail.com>
2019-02-14 13:40:28 -0500

    cleaning generate_label_params

--- src/params/generate_label_params.js
+++ src/params/generate_label_params.js
@@ -1,36 +1,37 @@
 var generate_ordered_labels = require('./../matrix_labels/generate_ordered_labels');
 module.exports = function generate_label_params(params){
 
-  params.labels = {};
-  params.labels.num_row = params.mat_data.length;
-  params.labels.num_col = params.mat_data[0].length;
+  var labels = {};
+  labels.num_row = params.mat_data.length;
+  labels.num_col = params.mat_data[0].length;
 
-  params.labels.offset_dict = {};
-  params.labels.draw_labels = false;
+  labels.offset_dict = {};
+  labels.draw_labels = false;
 
   // font_detail range: min ~12 max ~200
   // usable range: 14-30 (was using 25)
-  params.labels.font_detail = 40;
-
-  generate_ordered_labels(params);
+  labels.font_detail = 40;
 
   // generate titles if necessary
   var inst_label;
-  params.labels.titles = {};
-  params.labels.precalc = {}
+  labels.titles = {};
+  labels.precalc = {}
   _.each(['row', 'col'], function(inst_axis){
 
     // initialize with empty title
-    params.labels.titles[inst_axis] = '';
+    labels.titles[inst_axis] = '';
 
     inst_label = params.network[inst_axis + '_nodes'][0].name;
     if (inst_label.indexOf(': ') > 0){
-      params.labels.titles[inst_axis] = inst_label.split(': ')[0];
+      labels.titles[inst_axis] = inst_label.split(': ')[0];
     }
 
     // pre-calc text triangles if low enough number of labels
-    params.labels.precalc[inst_axis] = false;
+    labels.precalc[inst_axis] = false;
 
-  })
+  });
+
+  params.labels = labels;
+  generate_ordered_labels(params);
 
 };

551fa56d099b4f94db81fe4da26d6e50514a3203
Nick Fernandez <nickfloresfernandez@gmail.com>
2019-02-14 13:51:50 -0500

    cleaning gen_text_zoom_par

--- src/params/gen_text_zoom_par.js
+++ src/params/gen_text_zoom_par.js
@@ -1,18 +1,19 @@
 module.exports = function gen_text_zoom_par(params){
 
   var text_zoom = {};
+  var max_webgl_fs = {}
+  max_webgl_fs.row = 0.05;
+  max_webgl_fs.col = 0.06;
 
-  text_zoom.row = {};
-  text_zoom.row.scaled_num = params.labels.num_row;
-  text_zoom.row.reference = text_zoom.row.scaled_num;
-  text_zoom.row.factor = 1;
-  text_zoom.row.max_webgl_fs = 0.05;
+  _.each(['row', 'col'], function(inst_axis){
 
-  text_zoom.col = {};
-  text_zoom.col.scaled_num = params.labels.num_col;
-  text_zoom.col.reference = text_zoom.col.scaled_num;
-  text_zoom.col.factor = 1;
-  text_zoom.col.max_webgl_fs = 0.06;
+    text_zoom[inst_axis] = {}
+    text_zoom[inst_axis].scaled_num = params.labels['num_' + inst_axis];
+    text_zoom[inst_axis].reference = text_zoom[inst_axis].scaled_num;
+    text_zoom[inst_axis].factor = 1;
+    text_zoom[inst_axis].max_webgl_fs = max_webgl_fs[inst_axis];
+
+  });
 
   params.text_zoom = text_zoom;
 };

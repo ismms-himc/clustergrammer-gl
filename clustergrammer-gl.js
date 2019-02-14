8395fb5141ab88ef98f6859793fa6e59108859f3
Nick Fernandez <nickfloresfernandez@gmail.com>
2019-02-14 11:21:00 -0500

    cleaned up generate_animation_params

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -44943,27 +44943,21 @@ module.exports = function calc_viz_dim(regl, params){
 
 module.exports = function generate_animation_params(params){
 
-  // animation params
-  params.ani = {};
-  params.ani.time_remain = 0;
+  ani = {};
+  ani.time_remain = 0;
+  ani.running = false;
+  ani.run_animation = false;
+  ani.last_switch_time = 0;
+  ani.ani_duration = 3;
+  ani.duration_end = 0;
+  ani.time = 0;
+  ani.first_frame = true;
+  ani.ini_viz = true;
+  ani.last_click = 0;
+  ani.dblclick_duration = 0.5;
+  ani.update_viz = false;
 
-  params.ani.running = false;
-  params.ani.run_animation = false;
-
-  params.ani.last_switch_time = 0;
-  params.ani.ani_duration = 3;
-  params.ani.duration_end = 0;
-
-  params.ani.time = 0;
-  params.ani.first_frame = true;
-  params.ani.ini_viz = true;
-
-  params.ani.last_click = 0;
-  params.ani.dblclick_duration = 0.5;
-
-  // used to update viz after background calculations
-  params.ani.update_viz = false;
-
+  params.ani = ani;
 };
 
 /***/ }),

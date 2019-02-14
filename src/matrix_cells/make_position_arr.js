c9dce33c1dd918c2d11012c7c425258ae6f3bedb
Nick Fernandez <nickfloresfernandez@gmail.com>
2019-02-14 10:59:40 -0500

    further leaning make_position_arr

--- src/matrix_cells/make_position_arr.js
+++ src/matrix_cells/make_position_arr.js
@@ -1,8 +1,8 @@
 
 module.exports = function make_position_arr(params, inst_row_order, inst_col_order){
 
-  var num_row = params.labels.num_row;
-  var num_col = params.labels.num_col;
+  var labels = params.labels;
+  var canvas_pos = params.node_canvas_pos;
 
   // pass along row and col node information
   var row_nodes = params.network.row_nodes;
@@ -10,17 +10,16 @@ module.exports = function make_position_arr(params, inst_row_order, inst_col_ord
 
   var row_order_id;
   var col_order_id;
-  var canvas_pos = params.node_canvas_pos;
 
   function position_function(d, i){
 
-    row_order_id = num_row - 1 - row_nodes[Math.floor(i / num_col)][inst_row_order];
-    col_order_id = num_col - 1 - col_nodes[i % num_col][inst_col_order];
+    row_order_id = labels.num_row - 1 - row_nodes[Math.floor(i / labels.num_col)][inst_row_order];
+    col_order_id = labels.num_col - 1 - col_nodes[i % labels.num_col][inst_col_order];
 
     return [canvas_pos.x_arr[col_order_id], canvas_pos.y_arr[row_order_id]];
   }
 
-  var pos_arr = Array(num_row * num_col)
+  var pos_arr = Array(labels.num_row * labels.num_col)
             .fill()
             .map(position_function);
 

e1c789b8cba2c6e9260e9e184c4ca8375d2bbb85
cornhundred <nickfloresfernandez@gmail.com>
2019-02-14 21:33:18 -0500

    cleaned generate_ordered_labels

--- src/matrix_labels/generate_ordered_labels.js
+++ src/matrix_labels/generate_ordered_labels.js
@@ -1,47 +1,47 @@
-module.exports = function generate_ordered_labels(params){
+module.exports = function generate_ol(params){
 
   /*
     Generate lists of ordered label and category names for mouseover
   */
 
   var inst_order;
-  var ordered_labels = {};
+  var ol = {};
 
   var axis_nodes;
   var i;
 
-  _.each(['row', 'col'], function(inst_axis){
+  _.each(['row', 'col'], function(i_axis){
 
-    ordered_labels[inst_axis + 's'] = [];
-    ordered_labels[inst_axis + '_indices'] = [];
+    ol[i_axis + 's'] = [];
+    ol[i_axis + '_indices'] = [];
 
-    axis_nodes = params.network[inst_axis + '_nodes'];
+    axis_nodes = params.network[i_axis + '_nodes'];
 
     var found_axis_cat = false;
 
-    for (i = 0; i < params.cat_data.cat_num[inst_axis]; i++) {
-      ordered_labels[inst_axis + '_cats-' + String(i)] = [];
+    for (i = 0; i < params.cat_data.cat_num[i_axis]; i++) {
+      ol[i_axis + '_cats-' + String(i)] = [];
     }
 
-    if (params.cat_data.cat_num[inst_axis] > 0){
+    if (params.cat_data.cat_num[i_axis] > 0){
       found_axis_cat = true;
     }
 
     _.each(axis_nodes, function(inst_node, inst_index){
 
-      inst_order = params.labels['num_' + inst_axis] - 1 - inst_node[params.order.inst[inst_axis]];
+      inst_order = params.labels['num_' + i_axis] - 1 - inst_node[params.order.inst[i_axis]];
 
       // ordered names
-      ordered_labels[inst_axis + 's'][inst_order] = inst_node.name;
+      ol[i_axis + 's'][inst_order] = inst_node.name;
 
       // ordered indices (for value retrieval)
-      ordered_labels[inst_axis + '_indices'][inst_order] = inst_index;
+      ol[i_axis + '_indices'][inst_order] = inst_index;
 
       if (found_axis_cat){
 
-        for (i = 0; i < params.cat_data.cat_num[inst_axis]; i++) {
+        for (i = 0; i < params.cat_data.cat_num[i_axis]; i++) {
 
-          ordered_labels[inst_axis + '_cats-' + String(i)][inst_order] = inst_node['cat-' + String(i)];
+          ol[i_axis + '_cats-' + String(i)][inst_order] = inst_node['cat-' + String(i)];
 
         }
       }
@@ -50,6 +50,6 @@ module.exports = function generate_ordered_labels(params){
 
   });
 
-  params.labels.ordered_labels = ordered_labels;
+  params.labels.ordered_labels = ol;
 
 };

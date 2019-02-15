079e4f155b50dd10167d9ed7cf879206b1f97255
cornhundred <nickfloresfernandez@gmail.com>
2019-02-14 22:20:37 -0500

    cleaned find_mouseover_element

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -41437,8 +41437,6 @@ module.exports = function final_mouseover_frame(regl, params){
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-var get_mouseover_type = __webpack_require__(/*! ./get_mouseover_type */ "./src/interactions/get_mouseover_type.js");
-
 module.exports = function find_mouseover_element(regl, params, ev){
 
   /*
@@ -41452,37 +41450,37 @@ module.exports = function find_mouseover_element(regl, params, ev){
   var mouseover = params.int.mouseover;
 
   // reset mouseover params
-  _.each(['row', 'col'], function(inst_axis){
-    params.int.mouseover[inst_axis] = {};
-    params.int.mouseover[inst_axis].name = null;
-    params.int.mouseover[inst_axis].cats = [];
+  _.each(['row', 'col'], function(i_axis){
+    params.int.mouseover[i_axis] = {};
+    params.int.mouseover[i_axis].name = null;
+    params.int.mouseover[i_axis].cats = [];
   });
   params.int.mouseover.value = null;
 
   var offcenter = {};
-  var inst_cat_name;
+  var i_cat_name;
   var cursor_rel_min = {};
   var dim_dict = {};
   dim_dict.x = 'width';
   dim_dict.y = 'height';
 
-  _.each(['x', 'y'], function(inst_axis){
+  _.each(['x', 'y'], function(i_axis){
 
     // try updating mouseover position
-    params.zoom_data[inst_axis].cursor_position = ev[inst_axis + '0'];
+    params.zoom_data[i_axis].cursor_position = ev[i_axis + '0'];
 
     // convert offcenter WebGl units to pixel units
-    offcenter[inst_axis] = (params.viz_dim.canvas[dim_dict[inst_axis]] *
-                             params.viz_dim.offcenter[inst_axis])/2;
+    offcenter[i_axis] = (params.viz_dim.canvas[dim_dict[i_axis]] *
+                             params.viz_dim.offcenter[i_axis])/2;
 
-    cursor_rel_min[inst_axis] = params.zoom_data[inst_axis].cursor_position -
-                                  viz_dim_heat[inst_axis].min - offcenter[inst_axis];
+    cursor_rel_min[i_axis] = params.zoom_data[i_axis].cursor_position -
+                                  viz_dim_heat[i_axis].min - offcenter[i_axis];
 
-    cursor_rel_min[inst_axis] = cursor_rel_min[inst_axis] / params.zoom_data[inst_axis].total_zoom - params.zoom_data[inst_axis].total_pan_min;
+    cursor_rel_min[i_axis] = cursor_rel_min[i_axis] / params.zoom_data[i_axis].total_zoom - params.zoom_data[i_axis].total_pan_min;
 
   });
 
-  get_mouseover_type(params);
+  __webpack_require__(/*! ./get_mouseover_type */ "./src/interactions/get_mouseover_type.js")(params);
 
   var axis_indices = {};
 
@@ -41490,45 +41488,45 @@ module.exports = function find_mouseover_element(regl, params, ev){
 
     var axis_index;
 
-    var inst_dims = [];
+    var i_dims = [];
     if (params.tooltip.tooltip_type === 'matrix-cell'){
-      inst_dims = ['row', 'col'];
+      i_dims = ['row', 'col'];
     } else if (params.tooltip.tooltip_type.indexOf('row') >= 0){
-      inst_dims = ['row'];
+      i_dims = ['row'];
     } else if (params.tooltip.tooltip_type.indexOf('col') >= 0){
-      inst_dims = ['col'];
+      i_dims = ['col'];
 
       var y_heat_min = 126;
-      var inst_pix_y = params.zoom_data.y.cursor_position
-      var shift_col_label = y_heat_min - inst_pix_y;
+      var i_pix_y = params.zoom_data.y.cursor_position
+      var shift_col_label = y_heat_min - i_pix_y;
       if (shift_col_label > 0){
         cursor_rel_min.x = cursor_rel_min.x - shift_col_label/ params.zoom_data.x.total_zoom;
       }
     }
 
-    _.each(inst_dims, function(inst_axis){
+    _.each(i_dims, function(i_axis){
 
-      if (inst_axis === 'row'){
+      if (i_axis === 'row'){
         axis_index = Math.floor(cursor_rel_min.y/params.tile_pix_height);
-        axis_indices[inst_axis] = params.labels.ordered_labels[inst_axis + '_indices'][axis_index];
+        axis_indices[i_axis] = params.labels.ordered_labels[i_axis + '_indices'][axis_index];
       } else {
         axis_index = Math.floor(cursor_rel_min.x/params.tile_pix_width);
-        axis_indices[inst_axis] = params.labels.ordered_labels[inst_axis + '_indices'][axis_index];
+        axis_indices[i_axis] = params.labels.ordered_labels[i_axis + '_indices'][axis_index];
       }
 
-      mouseover[inst_axis].name = params.labels.ordered_labels[inst_axis + 's'][axis_index];
+      mouseover[i_axis].name = params.labels.ordered_labels[i_axis + 's'][axis_index];
 
-      if (typeof mouseover[inst_axis].name === 'string'){
-        if (mouseover[inst_axis].name.includes(': ')){
-          mouseover[inst_axis].name = mouseover[inst_axis].name.split(': ')[1];
+      if (typeof mouseover[i_axis].name === 'string'){
+        if (mouseover[i_axis].name.includes(': ')){
+          mouseover[i_axis].name = mouseover[i_axis].name.split(': ')[1];
         }
       }
 
       // reset cat names
-      mouseover[inst_axis].cats = [];
-      _.each(params.cat_data[inst_axis], function(d, cat_index){
-        inst_cat_name = params.labels.ordered_labels[inst_axis + '_cats-' + cat_index][axis_index];
-        mouseover[inst_axis].cats[cat_index] = inst_cat_name;
+      mouseover[i_axis].cats = [];
+      _.each(params.cat_data[i_axis], function(d, cat_index){
+        i_cat_name = params.labels.ordered_labels[i_axis + '_cats-' + cat_index][axis_index];
+        mouseover[i_axis].cats[cat_index] = i_cat_name;
       });
 
     });
@@ -41542,17 +41540,17 @@ module.exports = function find_mouseover_element(regl, params, ev){
   if (params.tooltip.tooltip_type.indexOf('dendro') >= 0){
 
     if (params.tooltip.tooltip_type === 'row-dendro'){
-      _.each(params.dendro.group_info.row, function(inst_group){
-        if (inst_group.all_names.includes(mouseover.row.name)){
-          mouseover.row.dendro = inst_group;
+      _.each(params.dendro.group_info.row, function(i_group){
+        if (i_group.all_names.includes(mouseover.row.name)){
+          mouseover.row.dendro = i_group;
         }
       });
     }
 
     if (params.tooltip.tooltip_type === 'col-dendro'){
-      _.each(params.dendro.group_info.col, function(inst_group){
-        if (inst_group.all_names.includes(mouseover.col.name)){
-          mouseover.col.dendro = inst_group;
+      _.each(params.dendro.group_info.col, function(i_group){
+        if (i_group.all_names.includes(mouseover.col.name)){
+          mouseover.col.dendro = i_group;
         }
       });
     }

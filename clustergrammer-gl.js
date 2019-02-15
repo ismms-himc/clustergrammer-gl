a7c964ddef14a234ab315694709912a5244425b6
cornhundred <nickfloresfernandez@gmail.com>
2019-02-14 21:44:44 -0500

    cleaning generate_cat_info

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -38975,18 +38975,12 @@ module.exports = function generate_cat_array(params, inst_axis){
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-var check_if_value_cats = __webpack_require__(/*! ./check_if_value_cats */ "./src/cats/check_if_value_cats.js");
-
 module.exports = function generate_cat_info(params){
 
-  // in clustergrammer.js this is make_cat_params
-
   var viz = {}
-
   var super_string = ': ';
   var tmp_super;
-  var inst_info;
-  // var inst_color;
+  var i_info;
 
   viz.show_categories = {};
   viz.all_cats = {};
@@ -39006,122 +39000,87 @@ module.exports = function generate_cat_info(params){
   viz.cat_colors.value_opacity = ini_val_opacity;
 
   // var num_colors = 0;
-  _.each(['row','col'], function(inst_rc){
+  _.each(['row', 'col'], function(i_rc){
 
-    viz.show_categories[inst_rc] = false;
+    viz.show_categories[i_rc] = false;
 
-    viz.all_cats[inst_rc] = [];
-    var tmp_keys = _.keys(params.network[inst_rc+'_nodes'][0]);
+    viz.all_cats[i_rc] = [];
+    var tmp_keys = _.keys(params.network[i_rc+'_nodes'][0]);
 
     tmp_keys = tmp_keys.sort();
 
     _.each( tmp_keys, function(d){
       if (d.indexOf('cat-') >= 0){
-        viz.show_categories[inst_rc] = true;
-        viz.all_cats[inst_rc].push(d);
+        viz.show_categories[i_rc] = true;
+        viz.all_cats[i_rc].push(d);
       }
     });
 
-    viz.cat_info[inst_rc] = null;
+    viz.cat_info[i_rc] = null;
+    viz.cat_colors[i_rc] = {};
+    viz.cat_info[i_rc] = {};
+    viz.cat_names[i_rc] = {};
 
-    // if (viz.show_categories[inst_rc]){
+    _.each( viz.all_cats[i_rc], function(cat_title){
 
-      viz.cat_colors[inst_rc] = {};
-      viz.cat_info[inst_rc] = {};
-      viz.cat_names[inst_rc] = {};
+      var i_node = params.network[i_rc+'_nodes'][0];
 
-      _.each( viz.all_cats[inst_rc], function(cat_title){
+      // look for title of category in category name
+      if (typeof i_node[cat_title] === 'string' ){
 
-        var inst_node = params.network[inst_rc+'_nodes'][0];
-
-        // look for title of category in category name
-        if (typeof inst_node[cat_title] === 'string' ){
-
-          if (inst_node[cat_title].indexOf(super_string) > 0){
-            tmp_super = inst_node[cat_title].split(super_string)[0];
-            viz.cat_names[inst_rc][cat_title] = tmp_super;
-          } else {
-            viz.cat_names[inst_rc][cat_title] = cat_title;
-          }
-
+        if (i_node[cat_title].indexOf(super_string) > 0){
+          tmp_super = i_node[cat_title].split(super_string)[0];
+          viz.cat_names[i_rc][cat_title] = tmp_super;
         } else {
-          viz.cat_names[inst_rc][cat_title] = cat_title;
+          viz.cat_names[i_rc][cat_title] = cat_title;
         }
 
-        var cat_instances_titles = _.pluck(params.network[inst_rc+'_nodes'], cat_title);
-        var cat_instances = [];
+      } else {
+        viz.cat_names[i_rc][cat_title] = cat_title;
+      }
 
-        _.each(cat_instances_titles, function(inst_cat){
+      var cat_instances_titles = _.pluck(params.network[i_rc+'_nodes'], cat_title);
+      var cat_instances = [];
 
-          var new_cat;
-          if (inst_cat.indexOf(': ') >0){
-            new_cat = inst_cat.split(': ')[1];
-          } else {
-            new_cat = inst_cat;
-          }
+      _.each(cat_instances_titles, function(i_cat){
 
-          cat_instances.push(new_cat);
-        });
-
-        var cat_states = _.uniq( cat_instances_titles ).sort();
-
-        // check whether all the categories are of value type
-        inst_info = check_if_value_cats(cat_states);
-
-        // add histogram to inst_info
-        if (inst_info.type === 'cat_strings'){
-          // remove titles from categories in hist
-          var cat_hist = _.countBy(cat_instances);
-          inst_info.cat_hist = cat_hist;
+        var new_cat;
+        if (i_cat.indexOf(': ') >0){
+          new_cat = i_cat.split(': ')[1];
         } else {
-          inst_info.cat_hist = null;
+          new_cat = i_cat;
         }
 
-        // pass info_info object
-        viz.cat_info[inst_rc][cat_title] = inst_info;
-
-        viz.cat_colors[inst_rc][cat_title] = {};
-
-        // _.each(cat_states, function(cat_tmp, inst_index){
-
-          // inst_color = colors.get_random_color(inst_index + num_colors);
-
-          // viz.cat_colors[inst_rc][cat_title][cat_tmp] = inst_color;
-
-          // // hack to get 'Not' categories to not be dark colored
-          // // also doing this for false
-          // if (typeof cat_tmp === 'string'){
-          //   if (cat_tmp.indexOf('Not ') >= 0 || cat_tmp.indexOf(': false') > 0){
-          //     viz.cat_colors[inst_rc][cat_title][cat_tmp] = '#eee';
-          //   }
-          // }
-
-          // num_colors = num_colors + 1;
-        // });
-
+        cat_instances.push(new_cat);
       });
 
-    // }
+      var cat_states = _.uniq( cat_instances_titles ).sort();
 
-    // if (_.has(params.network, 'cat_colors') && predefined_cat_colors === true){
-      viz.cat_colors[inst_rc] = params.network.cat_colors[inst_rc];
-    // }
+      // check whether all the categories are of value type
+      i_info = __webpack_require__(/*! ./check_if_value_cats */ "./src/cats/check_if_value_cats.js")(cat_states);
 
-    // if (params.sim_mat){
-    //   // sending row color info to columns since row color info can be updated
-    //   viz.cat_colors.col = viz.cat_colors.row;
-    // }
+      // add histogram to i_info
+      if (i_info.type === 'cat_strings'){
+        // remove titles from categories in hist
+        var cat_hist = _.countBy(cat_instances);
+        i_info.cat_hist = cat_hist;
+      } else {
+        i_info.cat_hist = null;
+      }
 
+      // pass info_info object
+      viz.cat_info[i_rc][cat_title] = i_info;
+      viz.cat_colors[i_rc][cat_title] = {};
+
+    });
+
+      viz.cat_colors[i_rc] = params.network.cat_colors[i_rc];
   });
-
-  // viz.cat_colors = viz.cat_colors;
 
   viz.cat_colors.opacity = 0.6;
   viz.cat_colors.active_opacity = 0.9;
 
   params.viz = viz;
-
-  // viz = calc_cat_params(params, viz);
 
 }
 

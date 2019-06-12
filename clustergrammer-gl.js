e6ae963d777652dc3d1a67a6771f647b151b72a0
cornhundred <nickfloresfernandez@gmail.com>
2019-06-12 14:40:06 -0400

    positioning tooltip

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -45232,23 +45232,63 @@ module.exports = function display_and_position_tooltip(params){
 
   params.d3_tip_width = d3_tip_width;
 
-
-  console.log('position tooltip type', params.tooltip.tooltip_type)
+  console.log('position tooltip type', params.tooltip.tooltip_type);
 
   // need to set up custom positioning of the tooltip based on the mouseover type
   // upper left if on matrix-cell, upper right if on row label, lower left if on
   // column mouseover. Should be able to check params.tooltip.tooltip_type to
   // find out how to position the tooltip
-  d3.select(params.tooltip_id)
-    .style('margin-left', function(){
-      var total_x_offset = params.zoom_data.x.cursor_position - d3_tip_width +
-                           magic_x_offset;
-      return total_x_offset + 'px'
-    })
-    .style('margin-top', function(){
-      var total_y_offset = params.zoom_data.y.cursor_position - d3_tip_height;
-      return total_y_offset + 'px'
-    })
+
+  // currently hardwiring positions
+
+  if (params.tooltip.tooltip_type === 'matrix-cell'){
+
+    d3.select(params.tooltip_id)
+      .style('margin-left', function(){
+        var total_x_offset = params.zoom_data.x.cursor_position - d3_tip_width +
+                             magic_x_offset;
+        return total_x_offset + 'px'
+      })
+      .style('margin-top', function(){
+        var total_y_offset = params.zoom_data.y.cursor_position - d3_tip_height;
+        return total_y_offset + 'px'
+      });
+
+  }
+  else if (params.tooltip.tooltip_type === 'col-label'){
+
+    d3.select(params.tooltip_id)
+      .style('margin-left', function(){
+        var total_x_offset = params.zoom_data.x.cursor_position - d3_tip_width +
+                             magic_x_offset;
+        return total_x_offset + 'px'
+      })
+      .style('margin-top', function(){
+
+        // var total_y_offset = params.zoom_data.y.cursor_position;
+        // var total_y_offset = params.viz_dim.heat.y.min;
+        var total_y_offset = 125;
+
+        return total_y_offset + 'px'
+      });
+  }
+
+  else if (params.tooltip.tooltip_type === 'row-label'){
+
+    d3.select(params.tooltip_id)
+      .style('margin-left', function(){
+        // var total_x_offset = params.zoom_data.x.cursor_position - d3_tip_width +
+        //                      magic_x_offset;
+        var total_x_offset = 150;
+
+        return total_x_offset + 'px'
+      })
+      .style('margin-top', function(){
+        var total_y_offset = params.zoom_data.y.cursor_position - d3_tip_height;
+        return total_y_offset + 'px'
+      });
+
+  }
 
 };
 

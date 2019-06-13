d1a8b6c2066330700b0ab50fbb9be6615ddb147e
cornhundred <nickfloresfernandez@gmail.com>
2019-06-13 14:33:55 -0400

    close button working on permanent tooltip on dendrogram

--- clustergrammer-gl.js
+++ clustergrammer-gl.js
@@ -47505,6 +47505,7 @@ module.exports = function initialize_d3_tip(params){
 
 var make_cat_breakdown_graph = __webpack_require__(/*! ./../cats/make_cat_breakdown_graph */ "./src/cats/make_cat_breakdown_graph.js");
 var calc_cat_cluster_breakdown = __webpack_require__(/*! ./../cats/calc_cat_cluster_breakdown */ "./src/cats/calc_cat_cluster_breakdown.js");
+var run_hide_tooltip = __webpack_require__(/*! ./run_hide_tooltip */ "./src/tooltip/run_hide_tooltip.js");
 
 module.exports = function make_dendro_tooltip(params, inst_axis){
 
@@ -47513,9 +47514,17 @@ module.exports = function make_dendro_tooltip(params, inst_axis){
   params.tooltip_fun.show('tooltip');
 
   d3.select(params.tooltip_id)
+    .append('div')
     .style('text-align', 'right')
+    .style('cursor', 'default')
+    .on('click', function(){
+      console.log('clicking close tooltip')
+      params.tooltip.permanent_tooltip = false;
+      run_hide_tooltip(params);
+    })
     .append('text')
     .text('X')
+    .style('font-size', '15px')
 
   var cat_breakdown = calc_cat_cluster_breakdown(params, mouseover[inst_axis].dendro, inst_axis);
   make_cat_breakdown_graph(params, mouseover[inst_axis].dendro, cat_breakdown);
--- src/tooltip/make_dendro_tooltip.js
+++ src/tooltip/make_dendro_tooltip.js
@@ -1,5 +1,6 @@
 var make_cat_breakdown_graph = require('./../cats/make_cat_breakdown_graph');
 var calc_cat_cluster_breakdown = require('./../cats/calc_cat_cluster_breakdown');
+var run_hide_tooltip = require('./run_hide_tooltip');
 
 module.exports = function make_dendro_tooltip(params, inst_axis){
 
@@ -8,9 +9,17 @@ module.exports = function make_dendro_tooltip(params, inst_axis){
   params.tooltip_fun.show('tooltip');
 
   d3.select(params.tooltip_id)
+    .append('div')
     .style('text-align', 'right')
+    .style('cursor', 'default')
+    .on('click', function(){
+      console.log('clicking close tooltip')
+      params.tooltip.permanent_tooltip = false;
+      run_hide_tooltip(params);
+    })
     .append('text')
     .text('X')
+    .style('font-size', '15px')
 
   var cat_breakdown = calc_cat_cluster_breakdown(params, mouseover[inst_axis].dendro, inst_axis);
   make_cat_breakdown_graph(params, mouseover[inst_axis].dendro, cat_breakdown);

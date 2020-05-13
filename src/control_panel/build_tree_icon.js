10a169a2412db75a8bb204550632fc6aa225d0af
cornhundred <nickfloresfernandez@gmail.com>
2020-05-13 01:28:44 -0400

    working on tree icon behavior

--- src/control_panel/build_tree_icon.js
+++ src/control_panel/build_tree_icon.js
@@ -50,6 +50,11 @@ module.exports = function build_tree_icon(cgm){
         d3.select(params.root + ' .run_cluster_container')
           .style('display', 'block')
 
+        d3.selectAll(params.root + ' .dist_options')
+          .style('display', 'block')
+        d3.selectAll(params.root + ' .link_options_container')
+          .style('display', 'block')
+
         params.viz.current_panel = 'recluster'
 
         console.log(params.viz.current_panel)
@@ -72,6 +77,11 @@ module.exports = function build_tree_icon(cgm){
         d3.selectAll(params.root + ' .reorder_buttons')
           .style('display', 'block');
         d3.select(params.root + ' .run_cluster_container')
+          .style('display', 'none')
+
+        d3.selectAll(params.root + ' .dist_options')
+          .style('display', 'none')
+        d3.selectAll(params.root + ' .link_options_container')
           .style('display', 'none')
 
         // toggle_menu(cgm, 'tree_menu', 'close');
@@ -196,14 +206,11 @@ module.exports = function build_tree_icon(cgm){
     })
     .attr('opacity', 0.0);
 
-  // var order_options = ['clust', 'sum', 'var'];
-
-  // Run Reorder Button
-  ///////////////////////////
   var control_panel_color = 'white';
   var text_color = '#47515b';
   var button_color = '#eee';
   var active_run_color = '#008000' //'#00FF75';
+  let active_button_color = '#008000'
 
   var button_dim = {};
   button_dim.height = 32;
@@ -212,17 +219,21 @@ module.exports = function build_tree_icon(cgm){
   button_dim.x_trans = button_dim.width + button_dim.buffer;
   button_dim.fs = 11;
 
-
+  ///////////////////////////
+  // Run Recluster Button
+  ///////////////////////////
   run_cluster_container = d3.select(params.root + ' .control_svg')
     .append('g')
-    .classed('run_cluster_container', true)
+    .classed('run_cluster_container', function(d){
+      console.log('SOMETHING')
+    })
     .attr('transform', 'translate('+ 350  +', '+ 91 +')')
     .on('click', function(d){
 
       console.log('clicking run button', d)
 
     })
-    .style('display', 'none')
+    .style('display', 'none');
 
   run_cluster_container
     .append('rect')
@@ -248,4 +259,143 @@ module.exports = function build_tree_icon(cgm){
     .style('cursor', 'default')
     .style('-webkit-user-select', 'none')
     .attr('transform', 'translate('+ button_dim.width/2 +', '+ button_dim.height/2 +')');
+
+  ///////////////////////////
+  // Recluster Options
+  ///////////////////////////
+
+  var dist_options = ['cos', 'corr', 'eucl'];
+
+  let dist_dict = {}
+  dist_dict['cosine'] = 'cos'
+  dist_dict['correlation'] = 'corr'
+  dist_dict['euclidean'] = 'eucl'
+
+  var cracker_room = 65;
+  var shift_x_order_buttons = 65 + cracker_room;
+  // button_groups.col.y_trans + button_dim.height + button_dim.buffer;
+
+  let y_offset_top = 47
+  let y_offset_bottom = 91
+
+  // Distance Options Container
+  ////////////////////////////////
+  dist_options = d3.select(params.root + ' .control_svg')
+    .append('g')
+    .classed('dist_option_container', true)
+    .selectAll('g')
+    .data(dist_options)
+    .enter()
+    .append('g')
+    .classed('dist_options', true)
+    .attr('transform', function(d, i){
+      var x_offset = button_dim.x_trans * i + shift_x_order_buttons;
+      return 'translate('+ x_offset  +', '+ y_offset_top +')';
+    })
+    .on('click', function(d){
+
+      console.log('clicking run button', d)
+
+    })
+    .style('display', 'none');
+
+  dist_options
+    .append('rect')
+    .style('height', button_dim.height)
+    .style('width', button_dim.width)
+    .style('fill', control_panel_color)
+    .style('rx', 10)
+    .style('ry', 10)
+    .attr('stroke', function(d){
+      var i_color;
+      if (dist_dict[params.matrix.distance_metric] == d){
+        i_color = active_button_color;
+      } else {
+        i_color = button_color;
+      }
+      return i_color;
+    })
+    .style('stroke-width', 2.5);
+
+  dist_options
+    .append('text')
+    .classed('button-name', true)
+    .text((d) => d.toUpperCase())
+    .style('font-family', '"Helvetica Neue", Helvetica, Arial, sans-serif')
+    .style('font-weight', 400)
+    .style('font-size', button_dim.fs)
+    .style('text-anchor', 'middle')
+    .style('stroke', text_color)
+    .style('alignment-baseline', 'middle')
+    .style('letter-spacing', '2px')
+    .style('cursor', 'default')
+    .style('-webkit-user-select', 'none')
+    .attr('transform', 'translate('+ button_dim.width/2 +', '+ button_dim.height/2 +')');
+
+  // Linkage Options Container
+  ////////////////////////////////
+  var link_options = ['avg', 'single', 'cmplt'];
+  let link_dict = {}
+  link_dict['average'] = 'avg'
+  link_dict['single'] = 'single'
+  link_dict['complete'] = 'cmplt'
+
+
+  link_options_container = d3.select(params.root + ' .control_svg')
+    .append('g')
+    .classed('link_option_container', true)
+    .selectAll('g')
+    .data(link_options)
+    .enter()
+    .append('g')
+    .classed('link_options_container', function(d){
+      console.log('HERE')
+      return true
+    })
+    .attr('transform', function(d, i){
+      var x_offset = button_dim.x_trans * i + shift_x_order_buttons;
+      return 'translate('+ x_offset  +', '+ y_offset_bottom +')';
+    })
+    .on('click', function(d){
+
+      console.log('clicking run button', d)
+
+    })
+    .style('display', 'none');
+
+  link_options_container
+    .append('rect')
+    .style('height', button_dim.height)
+    .style('width', button_dim.width)
+    .style('fill', control_panel_color)
+    .style('rx', 10)
+    .style('ry', 10)
+    .attr('stroke', function(d){
+      var i_color;
+      if (link_dict[params.matrix.linkage_type] == d){
+        i_color = active_button_color;
+      } else {
+        i_color = button_color;
+      }
+      return i_color;
+    })
+    .style('stroke-width', 2.5);
+
+  link_options_container
+    .append('text')
+    .classed('button-name', true)
+    .text((d) => d.toUpperCase())
+    .style('font-family', '"Helvetica Neue", Helvetica, Arial, sans-serif')
+    .style('font-weight', 400)
+    .style('font-size', button_dim.fs)
+    .style('text-anchor', 'middle')
+    .style('stroke', text_color)
+    .style('alignment-baseline', 'middle')
+    .style('letter-spacing', '2px')
+    .style('cursor', 'default')
+    .style('-webkit-user-select', 'none')
+    .attr('transform', 'translate('+ button_dim.width/2 +', '+ button_dim.height/2 +')');
+
+
 };
+
